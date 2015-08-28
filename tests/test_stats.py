451be92ce70529a01fec8e4775c16cdcd21c068d
mjamesruggiero <mjamesruggiero@gmail.com>
2015-08-27 17:33:51 -0700

    range and variance

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -5,12 +5,16 @@ import unittest
 
 from .context import tripp
 from tripp import stats
+import logging
+
+logging.basicConfig(level=logging.ERROR, format="%(lineno)d\t%(message)s")
 
 
 class TestStats(unittest.TestCase):
 
     def setUp(self):
         self.friends = [1, 3, 4, 5 ,2, 7, 5, 3, 13, 2, 9, 4]
+        self.longer_sequence = [1, 2, 3, 4, 66, 77, 88, 10034]
 
     def test_mean(self):
         """stats -- mean"""
@@ -32,3 +36,23 @@ class TestStats(unittest.TestCase):
         """stats -- mode"""
         result = stats.mode(self.friends)
         self.assertEqual([2, 3, 4, 5], result)
+
+    def test_data_range(self):
+        """stats -- data range"""
+        result = stats.data_range(self.longer_sequence)
+        self.assertEqual(10033, result)
+
+    def test_variance(self):
+        """stats -- variance"""
+        result = stats.variance(self.friends)
+        self.assertEqual(10.64, round(result, 2))
+
+    def test_standard_deviation(self):
+        """stats -- standard deviation"""
+        result = stats.standard_deviation(self.friends)
+        self.assertEqual(3.26, round(result, 2))
+
+    def test_interquartile_range(self):
+        """stats -- interquartile_range"""
+        result = stats.interquartile_range(self.friends)
+        self.assertEqual(4, result)
--- tripp/stats.py
+++ tripp/stats.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
 from collections import Counter
+from algebra import sum_of_squares
+import math
 import logging
 
 logging.basicConfig(level=logging.INFO, format="%(lineno)d\t%(message)s")
@@ -33,3 +35,22 @@ def mode(x):
     max_count = max(counts.values())
     return [i for i, count in counts.iteritems()
             if count == max_count]
+
+def data_range(elements):
+    """returns the difference
+    between largest and smallest elements"""
+    return max(elements) - min(elements)
+
+def variance(elements):
+    """assumes x has at least 2 elements"""
+    n = mean(elements)
+    dev2 = [(x - n)**2 for x in elements]
+    return mean(dev2)
+
+def standard_deviation(x):
+    return math.sqrt(variance(x))
+
+def interquartile_range(elements):
+    """the difference between
+    the 75th percentile and 25th percentile"""
+    return quantile(elements, 0.75) - quantile(elements, 0.25)
