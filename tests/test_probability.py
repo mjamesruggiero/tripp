93104fda0fda9bb48d8201db4846c7b26d79591c
mjamesruggiero <mjamesruggiero@gmail.com>
2015-09-02 18:13:08 -0700

    inverse normal CDF

--- tests/test_probability.py
+++ tests/test_probability.py
@@ -87,3 +87,44 @@ class TestProbability(unittest.TestCase):
                     0.2218416693589111,
                     0.1579003166017883]
         self.assertEqual(expected, result)
+
+    def test_inverse_normal_cdf(self):
+        """probability -- inverse normal cdf"""
+        # test case taken from http://bit.ly/1hAdejH
+        p_values = [0.0000001,
+                    0.00001,
+                    0.001,
+                    0.05,
+                    0.15,
+                    0.25,
+                    0.35,
+                    0.45,
+                    0.55,
+                    0.65,
+                    0.75,
+                    0.85,
+                    0.95,
+                    0.999,
+                    0.99999,
+                    0.9999999]
+        expected_inverse_normals = [-5.199337582187471,
+                                    -4.264890793922602,
+                                    -3.090232306167813,
+                                    -1.6448536269514729,
+                                    -1.0364333894937896,
+                                    -0.6744897501960817,
+                                    -0.38532046640756773,
+                                    -0.12566134685507402,
+                                    0.12566134685507402,
+                                    0.38532046640756773,
+                                    0.6744897501960817,
+                                    1.0364333894937896,
+                                    1.6448536269514729,
+                                    3.090232306167813,
+                                    4.264890793922602,
+                                    5.199337582187471]
+        for i, p in enumerate(p_values):
+            significant_digit = 3
+            result = round(probability.inverse_normal_cdf(p), significant_digit)
+            expected = round(expected_inverse_normals[i], significant_digit)
+            self.assertEqual(result, expected)
--- tripp/probability.py
+++ tripp/probability.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 import math
+import random
 
 logging.basicConfig(level=logging.ERROR, format="%(lineno)d\t%(message)s")
 
@@ -24,3 +25,40 @@ def normal_pdf(x, mu=0, sigma=1):
     """classic bell-shaped distribution"""
     sqrt_two_pi = math.sqrt(2 * math.pi)
     return (math.exp(-(x - mu) ** 2 / 2 / sigma ** 2) / (sqrt_two_pi * sigma))
+
+
+def normal_cdf(x, mu=0, sigma=1):
+    """the probability that a real-valued random variable X
+    with a given probability distribution will be found
+    to have a value less than or equal to x"""
+    return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
+
+
+def inverse_normal_cdf(p, mu=0, sigma=1, tolerance=0.00001):
+    """find approximate inverse using binary search"""
+    # if not standard, compute standard and rescale
+    if mu != 0 or sigma != 1:
+        return mu * sigma * inverse_normal_cdf(p, tolerance=tolerance)
+
+    low_z = -10.0  # normal_cdf(-10) is very close to 0
+    hi_z = 10.0   # normal_cdf(10) is very close to 1
+    while hi_z - low_z > tolerance:
+        mid_z = (low_z + hi_z) / 2  # find the midpoint
+        mid_p = normal_cdf(mid_z)   # and its corresponding cdf
+        if mid_p < p:
+            # midpoint still too low, search above
+            low_z = mid_z
+        elif mid_p > p:
+            # midpoint too high, search below
+            hi_z = mid_z
+        else:
+            break
+    return mid_z
+
+
+def bernoulli_trial(p):
+    return 1 if random.random() < p else 0
+
+
+def binomial(n, p):
+    return sum(bernoulli_trial(p) for _ in range(n))
