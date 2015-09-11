73597829d50136d8e0beb646c4b15740fd9fd2e7
mjamesruggiero <mjamesruggiero@gmail.com>
2015-09-10 18:18:05 -0700

    preliminary test

--- tests/test_hypothesis.py
+++ tests/test_hypothesis.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-
+import random
 from .context import tripp
 from tripp import hypothesis
 import logging
@@ -13,9 +13,6 @@ logging.basicConfig(level=logging.INFO, format="%(lineno)d\t%(message)s")
 
 class TestHypothesis(unittest.TestCase):
 
-    def setUp(self):
-        pass
-
     def test_normal_approximation_to_binomial(self):
         """hypothesis -- normal_approximation_to_binomial"""
         n = 1000
@@ -25,3 +22,17 @@ class TestHypothesis(unittest.TestCase):
         mu, sigma = hypothesis.normal_approximation_to_binomial(n, p)
         self.assertEqual(mu, expected_mu)
         self.assertEqual(round(sigma, 4), expected_sigma)
+
+    def test_p_hacking(self):
+        """hypothesis -- run_experiment"""
+        random.seed(0)
+        experiments = [hypothesis.run_experiment() for _ in range(1000)]
+        num_rejections = len([x for x in experiments
+                             if hypothesis.reject_fairness(x)])
+        logging.debug("num_rejections is {}".format(num_rejections))
+        self.assertEqual(num_rejections, 46)
+
+    def test_a_b_test_statistic(self):
+        """hypothesis -- a_b_test_statistic"""
+        result = hypothesis.a_b_test_statistic(1000, 200, 1000, 180)
+        self.assertEqual(round(result, 2), -1.14)
