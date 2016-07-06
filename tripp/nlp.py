f420c41d3271d82403fa8b6421888c009b991bdd
mjamesruggiero <mjamesruggiero@gmail.com>
2016-07-05 21:22:35 -0700

    Grammar

--- tripp/nlp.py
+++ tripp/nlp.py
@@ -86,8 +86,7 @@ def get_document(url):
         document.extend(words)
     return document
 
-def bigram_poc(url, count=10):
-    document = get_document(url)
+def get_bigram(document, count=10):
     bigrams = zip(document, document[1:])
     transitions = defaultdict(list)
     for prev, current in bigrams:
@@ -112,15 +111,44 @@ def get_trigram(document):
     generated = generate_using_trigrams(starts, trigram_transitions)
     return generated
 
+def is_terminal(token):
+    return token[0] != '_'
+
+def expand(grammar, tokens):
+    for i, token in enumerate(tokens):
+
+        # skip terminals
+        if is_terminal(token):
+            continue
+
+        # if you got here, we found a non-terminal token
+        # and we need to choose a random replacement
+        replacement = random.choice(grammar[token])
+
+        if is_terminal(replacement):
+            tokens[i] = replacement
+        else:
+            tokens = tokens[:i] + replacement.split() + tokens[(i + 1):]
+
+        # call yourself on the new list of tokens
+        return expand(grammar, tokens)
+
+    # if we get this far, it was all terminals
+    return tokens
+
+def generate_sentence(grammar):
+    return expand(grammar, ['_S'])
+
 
 
 if __name__ == '__main__':
-    TEST_METHODOLOGY = 'trigrams'
+    TEST_METHODOLOGY = 'grammars'
 
     url = "http://radar.oreilly.com/2010/06/what-is-data-science.html"
 
     if 'bigrams' == TEST_METHODOLOGY:
-        bigrams = bigram_poc(url, count=20)
+        document = get_document(url)
+        bigrams = get_bigram(document, count=20)
         for l in bigrams:
             logging.info("bigram -> {}".format(l))
 
@@ -129,3 +157,18 @@ if __name__ == '__main__':
         for i in range(0, 5):
             trigram_sentence = get_trigram(document)
             logging.info("trigram-> {}".format(trigram_sentence))
+
+    GRAMMAR = {
+        '_S': ['_NP _VP'],
+        '_NP': ['_N', '_A _NP _P _A _N'],
+        '_VP': ['_V', '_V _NP'],
+        '_N': ['data science', 'python', 'regression', 'engineering', 'management'],
+        '_A': ['big', 'linear', 'logistic', 'unwise', 'radical', 'conservative'],
+        '_P': ['about', 'near', 'behind', 'below', 'around', 'besides'],
+        '_V': ['learns', 'trains', 'leaves', 'develops', 'tests', 'is']
+    }
+
+    if 'grammars' == TEST_METHODOLOGY:
+        sentence = generate_sentence(GRAMMAR)
+        logging.info('(grammar): {}'.format(' '.join(sentence)))
+
